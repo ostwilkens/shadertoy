e79f910d88bbcf7376e572ff0f0642f2168a530c
ostwilkens <carl@ostwilkens.se>
2020-03-18 21:25:38 +0100

    stuf

--- 0_3.glsl
+++ 0_3.glsl
@@ -3,7 +3,7 @@
 #include "hg_sdf.glsl"
 
 #define PRECISION 1000.
-#define MAX_ITERATIONS 150
+#define MAX_ITERATIONS 250
 #define MAX_DIST 10.
 #define EPSILON (1. / PRECISION + 0.0001)
 const vec3 purple = normalize(vec3(0.298, 0.176, 0.459));
@@ -49,22 +49,36 @@ void sub(inout float d, inout vec3 c, float d2, vec3 color, float density, float
 
 vec4 scene(vec3 p)
 {
-    float n = iTime * 1. + 4.;
+    float n = iTime * 1.+ 4.;
     float d = 1. / 0.;
     vec3 c = vec3(0.);
 
     p.z -= 0.5;
 
+
+    // p.x += 0.5;
+    // pR(p.xz, sin(n) * 0.5);
+    add(d, c, fBoxCheap(p + vec3(0., 0., -3.), vec3(1., 1., 0.01)), purple.gbr, 1., 10.);
+
+
     pR(p.yz, 2.);
-    pR(p.xy, + n * 0.2);
+    pR(p.xy, PI/2. + sin(n) * 1.5);
     // pR(p.yz, + n * 0.2);
     // pR(p.zx, + n);
 
+    
+    // add(d, c, max(-fSphere(p, 0.15), fSphere(p, 0.15)), purple, 2., 5.);
     add(d, c, max(-fDodecahedron(p, 0.15), fDodecahedron(p, 0.15)), purple, 5., 20.);
+
+
+    // p.x += 0.4;
+
+    // add(d, c, fBox(p, vec3(0.1, 0.1, 0.1)), vec3(0., 1., 0.), 1., 1.);
+
 
     pR(p.yx,  n);
     p.z += tan(n * 2.) * 0.1;
-    // add(d, c, max(-fBox(p, vec3(0.2, 0.2, 0.2)), fBox(p, vec3(0.2, 0.2, 0.02))), purple.grb, 3., 0.4);
+    add(d, c, max(-fBox(p, vec3(0.2, 0.2, 0.2)), fBox(p, vec3(0.2, 0.2, 0.04))), purple.grb, 3., 0.4);
 
     // p.x -= 0.15;
     // add(d, c, fBox(p, vec3(0.1)), purple, 9., 1.);
@@ -77,19 +91,40 @@ vec4 scene(vec3 p)
     return vec4(c, d);
 }
 
-vec3 nor(in vec3 p)
+vec3 normalAt(in vec3 p)
 {
-    vec2 e = vec2(-EPSILON, EPSILON);   
-    float t1 = scene(p + e.yxx).w, t2 = scene(p + e.xxy).w;
-    float t3 = scene(p + e.xyx).w, t4 = scene(p + e.yyy).w;
+    vec2 e = vec2(-EPSILON, EPSILON);
+    float t1 = scene(p + e.yxx).w; 
+    float t2 = scene(p + e.xxy).w;
+    float t3 = scene(p + e.xyx).w;
+    float t4 = scene(p + e.yyy).w;
 
     return vec3(normalize(e.yxx*t1 + e.xxy*t2 + e.xyx*t3 + e.yyy*t4));
+}
+
+vec3 normalAt2(in vec3 p)
+{
+	vec3 normal;
+	vec3 ep = vec3(EPSILON, 0, 0);
+
+	normal.x = scene(p + ep.xyz).w - scene(p - ep.xyz).w;
+	normal.y = scene(p + ep.yxz).w - scene(p - ep.yxz).w;
+	normal.z = scene(p + ep.yzx).w - scene(p - ep.yzx).w;
+
+	return normalize(normal);
+}
+
+vec3 normalAt3(in vec3 p)
+{
+    float c = scene(p).w;
+	vec2 nOfs = vec2(0.001, 0.0);
+	return normalize(vec3(scene(p + nOfs.xyy).w, scene(p + nOfs.yxy).w, scene(p + nOfs.yyx).w) - c);
 }
 
 vec3 marchReflection(vec3 pos, vec3 rayDir)
 {
     vec3 c = vec3(0.);
-    vec3 normal = nor(pos);
+    vec3 normal = normalAt3(pos - rayDir * EPSILON * 2.);
     rayDir = reflect(rayDir, normal);
     
     for(int i = 0; i < MAX_ITERATIONS; i++)
@@ -130,16 +165,24 @@ vec3 march(vec2 uv)
         pos += rayDir * d;
         c += s.rgb / (20. + PRECISION);
 
-        // if(pos.z > 0.5)
-        if(reflected == 0. && d < EPSILON)
+        if(reflected <= 0. && d < EPSILON)
         {
-            reflected += 1.;
+            reflected = 1.;
 
-            c += marchReflection(pos, rayDir);
+            c += marchReflection(pos, rayDir) * 0.5;
+
+            // c.r = 1.;
+
+            // break;
 
             // rayDir = reflect(rayDir, normalize(rayDir));
             // c = vec3(1.);
         }
+        // else 
+        // {
+        //     reflected -= EPSILON * 100.;
+        // }
+        
 
         if(d > MAX_DIST)
             break;
